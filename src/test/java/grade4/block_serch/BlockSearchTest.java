f568819639d8a59083cf41bbd730d11e8ee361d7
刘成 <liucheng@xumaochengdeiMac.local>
2017-12-27 19:42:34 +0800

    develop

---
+++ src/test/java/grade4/block_serch/BlockSearchTest.java
@@ -0,0 +1,46 @@
+package grade4.block_serch; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* BlockSearch Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>十二月 20, 2017</pre> 
+* @version 1.0 
+*/ 
+public class BlockSearchTest { 
+
+    @Before
+    public void before() throws Exception { 
+    
+    } 
+
+    @After
+    public void after() throws Exception { 
+    } 
+
+        /** 
+    * 
+    * Method: createBlocks(int[] data, int blockSize) 
+    * 
+    */ 
+    @Test
+    public void testCreateBlocks() throws Exception { 
+    //TODO: Test goes here... 
+    } 
+    
+        /** 
+    * 
+    * Method: search(int[] data) 
+    * 
+    */ 
+    @Test
+    public void testSearch() throws Exception { 
+    //TODO: Test goes here... 
+    } 
+    
+    
+    } 
