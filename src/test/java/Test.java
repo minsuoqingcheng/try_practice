b8ee082caed07e3091ef56ac825111935f311fe8
imccl <1397004810@qq.com>
2018-01-11 21:25:50 +0800

    merge

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
 
-
+        List<Integer> tests = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            tests.add(i);
+        }
+        System.out.println(tests);
+        Collections.shuffle(tests);
+        System.out.println(tests);
 
     }
 
