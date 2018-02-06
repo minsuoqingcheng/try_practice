c74ec15fd9ee83a4d8587e50f85721a8e7ca5def
刘成 <liucheng@xumaochengdeiMac.local>
2018-02-06 21:45:51 +0800

    equals() Symmetric test

---
+++ src/main/java/grade4/equlas_test/CaseInsensitiveString.java
@@ -0,0 +1,24 @@
+package grade4.equlas_test;
+
+import java.util.Objects;
+
+public class CaseInsensitiveString {
+
+    private final String s;
+
+    public CaseInsensitiveString(String s) {
+        this.s = Objects.requireNonNull(s);
+    }
+
+    //违反了equals的对称性
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof CaseInsensitiveString) {
+            return s.equalsIgnoreCase(((CaseInsensitiveString) o).s);
+        }
+        if (o instanceof String) {
+            return s.equalsIgnoreCase((String) o);
+        }
+        return false;
+    }
+}
