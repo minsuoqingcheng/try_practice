641261bcc830985c63e82379f2d4c3e4f02da60f
Cheng Liu <chengliu@ChengdeiMac.local>
2018-03-29 09:16:28 +0800

    lambda first

---
+++ src/main/java/lambda/Operation.java
@@ -0,0 +1,26 @@
+package lambda;
+
+import java.util.function.DoubleBinaryOperator;
+
+public enum Operation {
+
+    PLUS("+", (x, y) -> x + y);
+
+
+    private final String symbol;
+    private final DoubleBinaryOperator operator;
+
+    Operation(String symbol, DoubleBinaryOperator operator) {
+        this.symbol = symbol;
+        this.operator = operator;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+
+    public double apply(double x, double y) {
+        return operator.applyAsDouble(x, y);
+    }
+}
