d5d5304ea2f7b71d89793bb51eb51c42bd2f0623
Cheng Liu <chengliu@ChengdeMacBook-Pro.local>
2018-04-10 21:34:27 +0800

    add zookeeper practice

---
+++ src/main/java/zookeeper/CreateGroup.java
@@ -0,0 +1,43 @@
+package zookeeper;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class CreateGroup implements Watcher {
+
+    private static final int SESSION_TIMEOUT = 5000;
+    private ZooKeeper zooKeeper;
+    private CountDownLatch connectedSingal = new CountDownLatch(1);
+
+    public void connect(String host) throws IOException, InterruptedException {
+        zooKeeper = new ZooKeeper(host, SESSION_TIMEOUT, this);
+        connectedSingal.await();
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+            connectedSingal.countDown();
+        }
+    }
+
+    public void create(String groupName) throws KeeperException, InterruptedException {
+        String path = "/" + groupName;
+        String createdPath = zooKeeper.create(path, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        System.out.println("Created: " + createdPath);
+    }
+
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        CreateGroup createGroup = new CreateGroup();
+        createGroup.connect("127.0.0.1");
+        createGroup.create("test");
+        createGroup.close();
+    }
+}
