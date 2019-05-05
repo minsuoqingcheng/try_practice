140af36a03fb021da2fb1f18a81ca5fa2689dbd3
chengliu <1397004810@qq.com>
2019-05-05 22:38:01 +0800

    fix error

--- src/main/java/generics/result/Client.java
+++ src/main/java/generics/result/Client.java
@@ -7,8 +7,8 @@ public class Client {
 
     public static void main(String[] args) {
 
-        ResultVO<Integer> resultVO = new ResultVO<>(0, Collections.singletonList(1));
-        ResultVO<Integer> resultVOData = new ResultVO<>(0, Arrays.asList(1, 2, 3));
+        ResultListVO<Integer> resultVO = new ResultListVO<>(0, Collections.singletonList(1));
+        ResultListVO<Integer> resultVOData = new ResultListVO<>(0, Arrays.asList(1, 2, 3));
 
         System.out.println(resultVO);
         System.out.println(resultVOData);
