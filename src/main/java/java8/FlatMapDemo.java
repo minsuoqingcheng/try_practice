94a14f6475fbe3680a98d29e6f9aeaaac8b5c533
chengliu <1397004810@qq.com>
2019-10-09 11:47:15 +0800

    flatMap test

--- src/main/java/java8/FlatMapDemo.java
+++ src/main/java/java8/FlatMapDemo.java
@@ -3,6 +3,7 @@ package java8;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlatMapDemo {
 
@@ -13,6 +14,15 @@ public class FlatMapDemo {
                 .flatMap(i -> numbers2.stream().map(j -> new int[]{i, j}))
                 .collect(Collectors.toList());
         pairs.forEach(p -> System.out.println(p[0]+" "+p[1]));
+
+
+        String test = "this is my test";
+        Stream.of(test.split(" ")).flatMap(s -> s.chars().boxed()).forEach(
+                i -> {
+                    System.out.print((char)i.intValue());
+                    System.out.print(" ");
+                }
+        );
     }
 
 }
